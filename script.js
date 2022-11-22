9a877eee1af2f3903932fe40765e5f8b61a24e9d
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-11-22 18:45:29 +0200

    Updates

---
+++ script.js
@@ -0,0 +1,18 @@
+// const img = document.getElementById('heart');
+
+// let toggle = true;
+
+// img.addEventListener('click', function() {
+//   toggle = !toggle;
+
+//   if (toggle) {
+//     // img.src = './images/Vector-hard.svg';
+//     img.setAttribute('src', './images/Vector-hard.svg');
+//   } else {
+//     // img.src = './images/red-hard.svg';
+//     img.setAttribute('src', './images/red-hard.svg');
+//   }
+//   console.log(img.src)
+// })
+
+// console.log(img.src)
